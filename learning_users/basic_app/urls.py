b5fa2f6425e6a8241822648141e58801919e60ce
Anilchoudary-Rugaramji <you@example.com>
2023-02-22 10:50:43 +0100

    first commit

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import re_path
+from basic_app import views
+
+
+# template tagging
+
+app_name = 'basic_app'
+
+urlpatterns = [
+
+    re_path(r'^register/$', views.register, name = 'register'),
+    re_path(r'^user_login/$', views.user_login, name = 'user_login'),
+
+
+
+]
